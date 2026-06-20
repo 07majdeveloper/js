d55e38a493511ff88cd09a8546e1715db83b55f3
Mohammed Abdul Jabbar <07majdeveloper@gmail.com>
2026-06-20 15:55:21 +0000

    datatypes summary

---
+++ 01_basics/summaryofdatatypes.js
@@ -0,0 +1,14 @@
+// non primitive data types:- ye wo hote hai jo muteable hote hai
+// 3types k data types hai 
+// Array=>["shaktiman","powerrangers","bheeem"]
+// function=> const function = (){} 
+// object => let user ={"name":farhan,age:20}
+
+// primitive datatypes ye immuteable hai 
+// string=>double quotes mein aata hai sirf ("farhan")
+// number=>number hota hai sirf (50)
+// Boolean:true/false=>haa ya naaa 
+// undefined=>future mein value aa sakti hai
+// null=>no value 
+// symbol=>unique (id1)
+// biginit => bade number hote hai (12358554369n)
